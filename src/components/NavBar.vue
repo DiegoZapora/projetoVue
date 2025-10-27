68f848ecdc99272337a3c0a01cc4a9ca5f4de8cd
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-10-27 10:35:54 -0300

    afsfas

---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="nav">
+        <RouterLink to="/">Home</RouterLink> /
+        <RouterLink to="/pedidos">Pedidos</RouterLink>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div id="home">
+        <h1>Bem-Vindo</h1>
+    </div>
+</template>
+
+<script>
+    
+</script>
